fa461f44a6fc5e8cba9ae28b236cda042256ade8
David Owens II <dowens@dowens-rmbp.(none)>
2012-09-06 23:20:34 -0700

    Start clean for Plexer v2

--- src/model/KSRegistration.h
+++
@@ -1,9 +0,0 @@
-//
-//  KSRegistration.h
-//  Plexer
-//
-//  Created by David Owens II on 7/20/09.
-//  Copyright 2009 Kiad Software. All rights reserved.
-//
-
-int isValidSerialNumber(const char* key);
